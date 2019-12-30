12c73f6a687e71d36e158a3369d97937fb1199ff
Kobe McKee <kobe.mckee@gmail.com>
2019-12-30 15:26:40 -0700

    Formatting url request

--- OSRS Guide/APIController.swift
+++ OSRS Guide/APIController.swift
@@ -19,13 +19,45 @@ class APIController {
     func searchItem(item: String) {
      
         var request = URLRequest(url: baseURL)
+        
+        //addValue? appendPathComponent?
         request.addValue("query", forHTTPHeaderField: "action")
         request.addValue("prop", forHTTPHeaderField: "revisions")
         request.addValue("rvprop", forHTTPHeaderField: "content")
         request.addValue("titles", forHTTPHeaderField: item)
         
-        print(request.allHTTPHeaderFields)
+        
+        //print(request.allHTTPHeaderFields)
     
+        request.httpMethod = "GET"
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if let error = error {
+                NSLog("Error running dataTask: \(error)")
+                print(error)
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned")
+                print("No data returned")
+                return
+            }
+            
+            print(String(decoding: data, as: UTF8.self))
+            
+            do {
+                let myData = try JSONDecoder().decode(Data.self, from: data)
+                
+                print(myData)
+                
+                
+            } catch {
+                NSLog("Error decoding data: \(error)")
+                print(error)
+            }
+        }.resume()
+        
         
     }
     
